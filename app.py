b021cbd4789f9b74ac4792f61501705081491f57
1leidark <leidark777@gmail.com>
2024-12-06 23:49:43 +0300

    Update app.py

--- app.py
+++ app.py
@@ -112,7 +112,7 @@ def about():
 def get_latest_data(existing_data):
     username = 'leidark777@gmail.com'
     password = 'lei777dark'
-        chrome_options = Options()
+    chrome_options = Options()
     chrome_options.add_argument("--headless")  # Убирает графический интерфейс (удобно для серверов)
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
