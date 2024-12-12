87e2b9b2b6ba7fe3ff75a7cab3bea57e43143a78
1leidark <leidark777@gmail.com>
2024-12-12 23:52:26 +0300

    Update benzin.py

--- benzin.py
+++ benzin.py
@@ -4,6 +4,7 @@ import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
